5cb8b5e8d9dd068994ffef9b0d15888023fa0a72
Romentos <padilla.naranja@gmail.com>
2023-10-02 13:01:25 +0200

    add ion-list

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface Components {
+  icon:string,
+  name:string,
+  redirectTo:string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -7,6 +14,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  components:Components[] = [
+    {
+      icon:'american-football-outline',
+      name:'Action Sheet',
+      redirectTo:'/action-sheet'
+    },
+    {
+      icon:'logo-apple-appstore',
+      name:'Alert',
+      redirectTo:'/alert'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
