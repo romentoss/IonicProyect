9c066d16e308a2434c1d4f6a727f5837bb4311cc
Romentos <padilla.naranja@gmail.com>
2023-10-04 14:11:46 +0200

    add ion list component ui

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
 import { DataService } from 'src/app/services/data.service';
+import { IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -11,10 +12,26 @@ export class ListPage implements OnInit {
 
   users!:Observable<any>;
 
+  @ViewChild(IonList) ionList!:IonList;
+
   constructor( private dataService:DataService) { }
 
   ngOnInit() {
     this.users = this.dataService.getUser();
+  }
+
+  share(user:any){
+    console.log('Shared',{user});
+    this.ionList.closeSlidingItems();
+  }
+  favorite(user:any){
+    console.log('Fab', {user});
+    this.ionList.closeSlidingItems();
+  }
+  delete(user:any){
+    console.log('Delete :',user.name);
+    this.ionList.closeSlidingItems();
+
   }
 
 }
